f39b7bdead3de14e0b9972a72f0ffddfd302b492
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-09 14:48:37 +0300

    q1

---
+++ main.js
@@ -0,0 +1,8 @@
+const user = {};
+
+user["email"] = "ivantarabanovych@egmail.com";
+user["user_name"] = "Ivan";
+user["user_age"] = "21";
+user["user_address"] = "Lviv, Lviv district, Hluniany, street ###"
+
+console.log(user);
