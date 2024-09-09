17e823bf7baa5610a91bdc38f15212262e158d6d
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-09 17:46:08 +0300

    q5

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const car = {
-    brand: "AUDI",
-    model: "A6",
+const product = {
+    name: "meat",
+    price: "120",
 
-    description: function(){
-        return `Марка автомобіля: ${this.brand}\nМодель автомобіля: ${this.model}`;
+    applyDiscount: function(discountPercentage){
+        const discountAmount = (this.price * discountPercentage) / 100;
+        this.price -= discountAmount;
+        console.log(`Знижка: ${discountPercentage}%\n Ціна після знижки: ${this.price.toFixed(2)}грн`);
     }
 };
 
-console.log(car.description());
+product.applyDiscount(30);
+
+
 
 
