5bdfcde6fa75175ca37c2af5013b7e0b4c0c2859
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-09 15:53:12 +0300

    q2

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const user = {};
+const calculator = {
+    add: function (a, b){
+        return a + b;
+    },
 
-user["email"] = "ivantarabanovych@egmail.com";
-user["user_name"] = "Ivan";
-user["user_age"] = "21";
-user["user_address"] = "Lviv, Lviv district, Hluniany, street ###"
+    subtract: function (a, b){
+        return a - b;
+    },
 
-console.log(user);
+    multiply: function (a, b){
+        return a * b;
+    },
+
+    divide: function (a, b){
+        if (b === 0){
+            return "Помилка ділення на 0"
+        }
+
+        return a / b;
+    }
+    
+};
+
+console.log(calculator.add (16, 4));
+console.log(calculator.subtract (16, 4));
+console.log(calculator.multiply (16, 4));
+console.log(calculator.divide (16, 0));
+console.log(calculator.divide (16, 4));
