d7f34045dd2d8b7c1c6564b97c4d5c75e5751617
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-09 23:02:46 +0300

    q6

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
-const product = {
-    name: "meat",
-    price: "120",
+const userProfile = {
+    username: "Ivan",
+    email: "ivantarabanovych@gmail.com",
+    password: "password123",
 
-    applyDiscount: function(discountPercentage){
-        const discountAmount = (this.price * discountPercentage) / 100;
-        this.price -= discountAmount;
-        console.log(`Знижка: ${discountPercentage}%\n Ціна після знижки: ${this.price.toFixed(2)}грн`);
+    changePassword: function(newPassword){
+        if(newPassword.length < 6){
+            console.error("Пароль має містити не менше 6 символів");
+            return;
+        }
+
+        const hasUpperCase = /[A-Z]/.test(newPassword);
+        const hasNamber = /[0-9]/.test(newPassword);
+        const hasSpecialChar = /[!@#$%^&*(),./?';']/.test(newPassword);
+
+        if(!hasUpperCase){
+            console.error("Пароль повинен містити хоча б одну велику літеру");
+            return;
+        }
+
+        if(!hasNamber){
+            console.error("Пароль повинен містити цифри");
+            return;
+        }
+
+        if(!hasSpecialChar){
+            console.error("Пароль повинен містити спеціальні символи");
+            return;
+        }
+
+        this.password = newPassword;
+        console.log("Пароль успішно змінений!");
     }
 };
 
-product.applyDiscount(30);
+userProfile.changePassword("mewp");
+userProfile.changePassword("password");
+userProfile.changePassword("Password");
+userProfile.changePassword("Password21");
+userProfile.changePassword("Password.21");
 
 
 
